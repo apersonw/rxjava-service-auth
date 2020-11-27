476b04f0cc0f64ecbb5cb9929477f7a527e3894e
happy <185408868@qq.com>
2020-11-28 02:50:31 +0800

    1、资源服务器

--- src/main/java/org/rxjava/service/auth/RxServiceAuthApplication.java
+++ src/main/java/org/rxjava/service/auth/RxServiceAuthApplication.java
@@ -1,6 +1,5 @@
 package org.rxjava.service.auth;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
