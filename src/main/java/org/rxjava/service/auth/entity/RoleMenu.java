c59609e6c3d06443cfad8e9eb467355e2f91c45b
happy <185408868@qq.com>
2021-01-24 23:30:32 +0800

    字典

---
+++ src/main/java/org/rxjava/service/auth/entity/RoleMenu.java
@@ -0,0 +1,40 @@
+package org.rxjava.service.auth.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class RoleMenu {
+    private String name;
+
+    /************通用定义字段************/
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+
+    @Version
+    private long version;
+
+    @CreatedBy
+    private String createUserId;
+
+    @LastModifiedBy
+    private String lastModifyUserId;
+
+    @CreatedDate
+    private LocalDateTime createDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDateTime;
+}
