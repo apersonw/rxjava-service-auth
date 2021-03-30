be14579828d20d18e5fefa9ed39e9cac76620b93
happy <185408868@qq.com>
2021-03-30 23:17:21 +0800

    1、修改错误

--- src/main/java/org/rxjava/service/auth/repository/UserRepository.java
+++ src/main/java/org/rxjava/service/auth/repository/UserRepository.java
@@ -2,6 +2,7 @@ package org.rxjava.service.auth.repository;
 
 import org.rxjava.service.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author happy
