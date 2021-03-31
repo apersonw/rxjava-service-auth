44b6cc1446869792cad5bd767fefe2c6befb1433
happy <185408868@qq.com>
2021-03-31 23:36:27 +0800

    1、主要控制器层

---
+++ src/main/java/org/rxjava/service/auth/controller/RoleController.java
@@ -0,0 +1,9 @@
+package org.rxjava.service.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("role")
+public class RoleController {
+}
