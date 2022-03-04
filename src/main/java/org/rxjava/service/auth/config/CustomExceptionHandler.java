90eb97778f3ba8207f9124da5b519aee64cd0679
happy <wugang@gmrfid.com>
2022-03-04 11:03:29 +0800

    修改异常为jdk17的代码风格

--- src/main/java/org/rxjava/service/auth/config/CustomExceptionHandler.java
+++ src/main/java/org/rxjava/service/auth/config/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.rxjava.service.auth.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
